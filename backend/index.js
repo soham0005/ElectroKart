5902517dcb7375fa22c15c858bb9fb0c89e1c31c
soham <sohamadhyapak0005@gmail.com>
2024-10-04 19:59:56 +0530

    File Structure rearranged

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,11 @@ app.post('/login', async (req, res) => {
 });
 
 
+app.get("/hello",async(req,res)=>{
+  console.log("Inside Hello GET Request");
+  return res.json({"message":"Success"}).status(200);
+})
+
 
 app.listen(3001, () => {
   console.log("Server listening on http://127.0.0.1:3001");
