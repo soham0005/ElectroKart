d0ddad0a51fc08348de94b29bdc4e1eb5acadc64
Rahul <rahulmudunuri12@gmail.com>
2024-10-06 13:42:46 +0530

    feat: added swipe option for touch devices

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,35 +1,65 @@
-import React from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/slider.css';
 
+const Banner = ({ banner }) => {
+    const [touchPosition, setTouchPosition] = useState(null);
 
-const Banner = ({banner}) => {
-    return (
-     
- <Carousel fade  >
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX;
+        setTouchPosition(touchDown);
+    };
 
- {banner.end.map((item,index)=>(
+    const handleTouchMove = (e) => {
+        if (touchPosition === null) {
+            return;
+        }
+
+        const currentTouch = e.touches[0].clientX;
+        const diff = touchPosition - currentTouch;
+
+        if (diff > 5) {
+            document.querySelector('.carousel-control-next').click();
+        }
+
+        if (diff < -5) {
+            document.querySelector('.carousel-control-prev').click();
+        }
+
+        setTouchPosition(null);
+    };
+
     
-    <Carousel.Item key={item.image}  id="banner" interval={1000} keyboard={true}>
-<img
-className="d-block w-100 "
-id="bannerImage"
-src={item.image}
-alt={`${index} banner`}
-/>
-<Carousel.Caption>
-<h3>{item.name}</h3>
-<p>{item.description}</p>
-<p>{item.source}</p>
-<u>Read more</u>
-</Carousel.Caption>
-</Carousel.Item>
 
- ))}
+    return (
+        <Carousel
+            onTouchStart={handleTouchStart} 
+            onTouchMove={handleTouchMove} 
+            interval={5000} 
+            keyboard={true}
+            pause="hover"
+        >
+            {banner.end.map((item, index) => (
+                
+                
+                <Carousel.Item key={item.image} id="banner">
+                    <img
+                        className="d-block w-100"
+                        id="bannerImage"
+                        src={item.image}
+                        alt={`${index} banner`}
+                    />
+                    <Carousel.Caption>
+                        <h3>{item.name}</h3>
+                        <p>{item.description}</p>
+                        <p>{item.source}</p>
+                        <u>Read more</u>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
+};
 
-
-</Carousel>
-    )
-}
-
-export default Banner
+export default Banner;
--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,24 +1,56 @@
-import { Carousel} from 'react-bootstrap'
-import React from 'react'
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/slider.css';
 
-function Slider({start}) {
-  return (
-    <Carousel fade>
 
-    {
-        start.map((item,index)=>(
-            <Carousel.Item key={index}>
-                <img className='d-block w-100'
-                src={item}
-                alt='First Slide'
-            />
-            </Carousel.Item>
-        ))
+function Slider({ start }) {
+  const [touchPosition, setTouchPosition] = useState(null);
+
+  const handleTouchStart = (e) => {
+    const touchDown = e.touches[0].clientX;
+    setTouchPosition(touchDown);
+  };
+
+  const handleTouchMove = (e) => {
+    if (touchPosition === null) {
+      return;
     }
-        
+
+    const currentTouch = e.touches[0].clientX;
+    const diff = touchPosition - currentTouch;
+
+    if (diff > 5) {
+      document.querySelector('.carousel-control-next').click();
+    }
+
+    if (diff < -5) {
+      document.querySelector('.carousel-control-prev').click();
+    }
+
+    setTouchPosition(null);
+  };
+  
+
+  return (
+    <Carousel
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      interval={3000}
+      pause="hover"
+      keyboard={true}
+    >
+      {start.map((item, index) => (
+        <Carousel.Item key={index}>
+          <img
+            className="d-block w-100"
+            src={item}
+            alt={`Slide ${index}`}
+          />
+        </Carousel.Item>
+      ))}
     </Carousel>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
