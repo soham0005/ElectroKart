a1677997dfa38731386ac5fefbd0b52f50a190a6
Ayush <asharma7588@gmail.com>
2024-10-06 14:40:30 +0530

    Favicon

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import '../styles/ItemCard.css';
+import React from "react";
+import "../styles/ItemCard.css";
 
-function ItemCard({image,index,name,price}) {
+function ItemCard({ image, index, name, price }) {
   return (
     <div className="ItemCard">
-            <img src={image} alt={`${index} product`} />
-            <p>{name}</p>
-            <span>{price}</span>
-            
-        </div>
-  )
+      <img src={image} alt={`${index} product`} loading="lazy" />
+      <p>{name}</p>
+      <span>{price}</span>
+    </div>
+  );
 }
 
-export default ItemCard
+export default ItemCard;
--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,24 +1,22 @@
-import { Carousel} from 'react-bootstrap'
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from "react-bootstrap";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Slider({start}) {
+function Slider({ start }) {
   return (
     <Carousel fade>
-
-    {
-        start.map((item,index)=>(
-            <Carousel.Item key={index}>
-                <img className='d-block w-100'
-                src={item}
-                alt='First Slide'
-            />
-            </Carousel.Item>
-        ))
-    }
-        
+      {start.map((item, index) => (
+        <Carousel.Item key={index}>
+          <img
+            className="d-block w-100"
+            src={item}
+            alt="First Slide"
+            loading="lazy"
+          />
+        </Carousel.Item>
+      ))}
     </Carousel>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
