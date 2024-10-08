17735314883aefc2f4fd4b041710c1f1a1778f81
Vishesh Tripathi <tripathiishu467@gmail.com>
2024-10-08 14:50:37 +0530

    loginPage

--- frontend/src/components/AllRoutes.jsx
+++ frontend/src/components/AllRoutes.jsx
@@ -6,6 +6,8 @@ import Login from './User/Login.jsx';
 import Home from './Home.jsx';
 import UserLogin from './Userlogin.jsx';
 import FAQs from './FAQ-feature/FAQ.jsx';
+import SignInPage from './User/Login.jsx';
+import SignupForm from './User/Signup.jsx';
 
 const AllRoutes = () => {
   return (
@@ -13,8 +15,8 @@ const AllRoutes = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/FAQs" element={<FAQs />} />
-        <Route exact path='/register' element={<UserLogin/>} />
-        <Route exact path="/login" element={<Login />} />
+        <Route exact path='/register' element={<SignupForm/>} />
+        <Route exact path="/login" element={<SignInPage/>} />
         <Route exact path="/music" element={<Accessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music} />} />
         <Route exact path="/smartDevices" element={<Accessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
         <Route exact path="/home" element={<Accessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
