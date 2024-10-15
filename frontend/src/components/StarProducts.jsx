93665bc9e6e8554b3aeb9d7a0c732d88954ef632
Prachi_18 <126897302+18Prachi@users.noreply.github.com>
2024-10-15 16:08:17 +0530

    Fixed the alignment of the pictures in the Features section

--- frontend/src/components/StarProducts.jsx
+++ frontend/src/components/StarProducts.jsx
@@ -4,27 +4,11 @@ import "../styles/StarProduct.css";
 function StarProducts({ starProduct }) {
   return (
     <div className="starProduct">
-      <div>
-        {" "}
-        <a href={starProduct[0].url}>
-          {" "}
-          <img src={starProduct[0].image} alt="1st Product" loading="lazy" />
+      {starProduct.map((product, index) => (
+        <a href={product.url} key={index}>
+          <img src={product.image} alt={`Product ${index + 1}`} loading="lazy" />
         </a>
-      </div>
-      <div>
-        <a href={starProduct[1].url}>
-          {" "}
-          <img src={starProduct[1].image} alt="1st Product" loading="lazy" />
-        </a>
-        <a href={starProduct[2].url}>
-          {" "}
-          <img src={starProduct[2].image} alt="1st Product" loading="lazy" />
-        </a>
-        <a href={starProduct[3].url}>
-          {" "}
-          <img src={starProduct[3].image} alt="1st Product" loading="lazy" />
-        </a>
-      </div>
+      ))}
     </div>
   );
 }
