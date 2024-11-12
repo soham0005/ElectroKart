3d9e9ca38ba0a1a4c50ca66c6816e4d89341bd6e
adityapatil-03 <125742752+adityapatil-03@users.noreply.github.com>
2024-11-12 11:32:27 +0530

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,6 +2,7 @@ const Product = require('../models/productsModel.js')
 const productRouter = require('express').Router();
 const { requireSignIn } = require('../middlewares/authMiddleware.js');
 
+
 productRouter.post('/products', requireSignIn, async (req, res) => {
     const { name, price, description, imageUrl } = req.body;
     try {
@@ -17,6 +18,16 @@ productRouter.get('/products', async (req, res) => {
     try {
         const products = await Product.find();
         res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+productRouter.get('/cart', async (req, res) => {
+    try {
+        const id = req.id;
+       // const products = await Product.find();
+        res.json(cart[id]);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
